77efec7516780aaa4ffaa6bc702659e1c43a384b
Bhawna11agg <bhawna11agg@gmail.com>
2019-12-17 17:09:40 +0700

    basic boilerplate for the android library

---
+++ elogbook/src/main/java/com/bhavin/elogbook/ElogDebug.java
@@ -0,0 +1,12 @@
+package com.bhavin.elogbook;
+
+import android.util.Log;
+
+public class ElogDebug {
+
+    private static final String TAG = "Elog Book";
+
+    public static void d(String message){
+        Log.d(TAG,message);
+}
+}
